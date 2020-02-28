23edbbdbc44f5b33f74f34559c801b18e7d0405a
Liz Fong-Jones <lizf@honeycomb.io>
2020-02-28 18:04:44 -0500

    build {arm64,amd64} debs. (#146)
    * Uses github tag publication workflow instead of s3 bucket workflow for publishing debs.
    * builds for arm and amd64.

--- build-pkg.sh
+++ build-pkg.sh
@@ -4,11 +4,11 @@
 set -e
 
 function usage() {
-    echo "Usage: build-pkg.sh -v <version> -t <package_type>"
+    echo "Usage: build-pkg.sh -m <arch> -v <version> -t <package_type>"
     exit 2
 }
 
-while getopts "v:t:" opt; do
+while getopts "v:t:m:" opt; do
     case "$opt" in
     v)
         version=$OPTARG
@@ -16,20 +16,27 @@ while getopts "v:t:" opt; do
     t)
         pkg_type=$OPTARG
         ;;
+    m)
+        arch=$OPTARG
+        ;;
     esac
 done
 
-if [ -z "$version" ] || [ -z "$pkg_type" ]; then
+if [ -z "$pkg_type" ] || [ -z "$arch" ]; then
     usage
+fi
+
+if [ -z "$version" ]; then
+    version=v0.0.0-dev
 fi
 
 fpm -s dir -n honeytail \
     -m "Honeycomb <team@honeycomb.io>" \
-    -p $GOPATH/bin \
     -v $version \
     -t $pkg_type \
+    -a $arch \
     --pre-install=./preinstall \
-    $GOPATH/bin/honeytail=/usr/bin/honeytail \
+    $GOPATH/bin/honeytail-linux-${arch}=/usr/bin/honeytail \
     ./honeytail.upstart=/etc/init/honeytail.conf \
     ./honeytail.service=/lib/systemd/system/honeytail.service \
     ./honeytail.conf=/etc/honeytail/honeytail.conf
